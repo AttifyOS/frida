8da25ceb0af5570fd4153bcb9b441b768329db3e
extremecoders-re <extremecoders@hotmail.com>
2022-11-14 14:47:56 +0530

    Frida 16.0.2

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ check_env() {
 install() {
   wget https://github.com/indygreg/python-build-standalone/releases/download/20220802/cpython-3.9.13+20220802-x86_64-unknown-linux-gnu-install_only.tar.gz -O $APM_TMP_DIR/cpython-3.9.13.tar.gz
   tar xf $APM_TMP_DIR/cpython-3.9.13.tar.gz -C $APM_PKG_INSTALL_DIR
-  $APM_PKG_INSTALL_DIR/python/bin/pip3.9 install frida-tools==11.0.0
+  $APM_PKG_INSTALL_DIR/python/bin/pip3.9 install frida==16.0.2 frida-tools==12.0.1
   ln -s $APM_PKG_INSTALL_DIR/python/bin/frida* $APM_PKG_BIN_DIR/
 }
 
